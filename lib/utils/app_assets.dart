c249d56101d8b4ac01ca2db0655572eee051f1a5
Jusy Lopes <jusylopes@outlook.com>
2025-07-29 23:43:12 -0300

    feat: update assets, splash screen and colors

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,5 @@
+abstract class AppAssets {
+  static const String _imagePath = 'assets/images/';
+  static String get background => '${_imagePath}background.png';
+  static String get pixelButton => '${_imagePath}pixel_button.png';
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color get primary => Color(0xFF00A0E9);
+  static Color get secondary => Color(0xFF0086DA);
+  static Color get accent => Color(0xFFFDBB00);
+}
